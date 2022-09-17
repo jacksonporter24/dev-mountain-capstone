2e07bfbedc8edf96ad0b8ab451407891862ecfdf
Jackson Porter <readjacksonporter@gmail.com>
2022-09-17 15:27:30 -0600

    clean post

--- src/form/form.jsx
+++ src/form/form.jsx
@@ -1,21 +1,5 @@
-// import React from "react";
+import React from "react";
 
-// export const Form = ({ newBook, setNewBook, addBook }) => {
-//   return (
-//     <form>
-//       <p>
-//         Title:
-//         <input
-//           value={newBook.bookInfo.title}
-//           onChange={(e) => {
-//             setNewBook({
-//               ...newBook,
-//               title: { ...newBook.title },
-//             });
-//           }}
-//         />
-//       </p>
-//     </form>
-//   );
-// };
-// export default Form;
+const Form = () => {};
+
+export default Form;
--- src/pages/book.jsx
+++ src/pages/book.jsx
@@ -9,38 +9,12 @@ function Book() {
   const [newTitle, setNewTitle] = useState([...data]);
   const [newDescription, setNewDescription] = useState([...data]);
 
-  // const addUser = (e) => {
-  //   e.preventDefault();
-  //   setUsers([...users, newUser]);
-  //   setShow(!show);
-  // };
-
-  // const handleCreateBook = (e) => {
-  //   e.preventDefault();
-  //   axios
-  //     .post("/api/books")
-  //     .then((res) => setNewBook([...newBook, res.setNewBook]));
-  // };
-
-  // const handleCreateBook = (description) => {
-  //   axios
-  //     .post("/api/books", {
-  //       userId: "345",
-  //       title: "This is a post title",
-  //       description: { description },
-  //     })
-  //     .then((response) => console.log(response.data)); //new get and line 25
-  // };
-
   const handleTitleInput = (event) => {
     setNewTitle(event.target.value);
-
-    // console.log("value is:", event.target.value);
   };
 
   const handleDescriptionInput = (event) => {
     setNewDescription(event.target.value);
-    // console.log('value description is', event.target.value)
   };
 
   const handleClick = (event) => {
@@ -51,20 +25,13 @@ function Book() {
         title: newTitle,
         description: newDescription,
       })
-      .then((response) => console.log(response.data));
-    axios.get("/api/books").then((res) => setData(res.data));
-
-    // console.log("final click", newTitle, newDescription);
+      .then((res) => setData(res.data));
   };
 
   useEffect(() => {
     axios.get("/api/books").then((res) => setData(res.data));
     console.log(data);
   }, []);
-
-  // useEffect(() => {
-  //   axios.post("/api/books").then((res) => )
-  // })
 
   return (
     <div>
@@ -85,7 +52,6 @@ function Book() {
             id="title-input"
             name="title-input"
             onChange={handleTitleInput}
-            value={newTitle}
           ></input>
           <p>INPUT DESCRIPTION</p>
           <input
@@ -93,27 +59,11 @@ function Book() {
             id="desc-input"
             name="desc-input"
             onChange={handleDescriptionInput}
-            value={newDescription}
+            // value={newDescription} //this would be required without useState
           ></input>
           <button onClick={handleClick}>Click</button>
-
-          {/* <button onClick={handleCreateBook}>SUBMIT TITLE</button>
-          <p>INPUT DESCRIPTION:</p>
-          <input onChange={handleCreateBook}></input>
-          <button>SUBMIT DESCRIPTION</button> */}
         </div>
       </div>
-      {/* // <input
-  //         value={newUser.name.first}
-  //         name="first"
-  //         onChange={(e) => {
-  //           setNewUser({
-  //             ...newUser,
-  //             name: { ...newUser.name, first: e.target.value },
-  //           });
-  //         }} */}
-
-      <div></div>
     </div>
   );
 }
